e72d9cc3be9085098bba63913d2e9c7555ba897b
Bruno Fernandes da Cunha Santos <fernandesbruno3232@hotmail.com>
2022-07-19 17:32:08 -0300

    Importação do Front-end Spring Boot

---
+++ README.md
@@ -0,0 +1,2 @@
+# Desafio-Final
+ Desafio do Oitavo Sprint - Iniciação a Fullstack - Curso de Formação em TI Senai Cimatec
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiURL: 'https://desafio-final-api-springboot.herokuapp.com'
+};
